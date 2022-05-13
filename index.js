16c4abec6d231fe38f7ac8c76b97df004b427721
jason decker <jdeck@MacBook-Pro.local>
2022-05-13 16:15:04 -0400

    Completed tasks 8-9

--- index.js
+++ index.js
@@ -316,7 +316,7 @@ function lotsOfArt(array) {
   }
   return newArr;
 }
-console.log(lotsOfArt(artists));
+// console.log(lotsOfArt(artists));
 
 
 
@@ -330,8 +330,12 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(array, para1){
+  for(let i = 0; i < array.length; i++){
+    if(array[i].name === para1){
+      return array[i].bio;
+    }
+  }
 }
 
 
@@ -345,8 +349,14 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, para1){
+  const newArr = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].nationality === para1){
+      newArr.push(array[i].name);
+    }
+  }
+  return newArr;
 }
 
 
